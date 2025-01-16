54315ff5ada68064a438fafa2e5e8605139cb2b3
nrhidayatiksm <myhidayatiksm@gmail.com>
2025-01-16 22:34:35 +0800

    add delete func

--- database/seeders/ApplicantInfoSeeder.php
+++ database/seeders/ApplicantInfoSeeder.php
@@ -38,5 +38,31 @@ class ApplicantInfoSeeder extends Seeder
             'Applicant_Marital' => 'Married',
             'Applicant_Mualaf' => 1,
         ]);
+        
+        Applicant_Info::create([
+            'User_ID' => 5,
+            'Applicant_DOB' => '2021-12-12',
+            'Applicant_Race' => 'Malay',
+            'Applicant_Citizenship' => 'Malaysian',
+            'Applicant_Address' => '392, Jalan Kamoung',
+            'Applicant_EduLevel' => 'Master',
+            'Applicant_EmpInfo' => 'Software Developer ',
+            'Applicant_Income' => 50000.00,
+            'Applicant_Marital' => 'Single',
+            'Applicant_Mualaf' => 1,
+        ]);
+        
+        Applicant_Info::create([
+            'User_ID' => 7,
+            'Applicant_DOB' => '2024-04-04',
+            'Applicant_Race' => 'Malay',
+            'Applicant_Citizenship' => 'Malaysian',
+            'Applicant_Address' => 'Jalan Poloh, Kampung Durian Runtuh',
+            'Applicant_EduLevel' => 'Spm',
+            'Applicant_EmpInfo' => 'Teacher',
+            'Applicant_Income' => 1350.00,
+            'Applicant_Marital' => 'Single',
+            'Applicant_Mualaf' => 1,
+        ]);
     }
 }
